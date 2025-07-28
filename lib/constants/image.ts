f2d986eaacbb02a123e92a2ec16ba2b967a64504
tesguru <olaogunteslim@gmail.com>
2025-07-28 16:34:58 +0100

    push chnages

--- lib/constants/image.ts
+++ lib/constants/image.ts
@@ -5,7 +5,7 @@ import IvantageLogo from "../../assets/images/ivantagelogo1.svg";
 import doctor from "../../assets/images/image171.png";
 import apple from "../../assets/images/pngwing1.png";
 import google from "../../assets/images/pngwing2.png";
-import cardBackground from "../../assets/images/card-1.png"
+import cardBackground from "../../assets/images/card-2.png"
 import sidebarlogo from "../../assets/images/ivantagelogo5.png"
 
 
