50a8b3646e68e06ab5cfdb366836bc49d26203f7
tesguru <olaogunteslim@gmail.com>
2025-07-28 16:25:34 +0100

    push chnages

--- lib/constants/image.ts
+++ lib/constants/image.ts
@@ -1,8 +1,8 @@
-import Woman from "../../assets/images/image 17.png";
-import Man from "../../assets/images/image 15.svg";
-import IvantageLogo2 from "../../assets/images/ivantage logo 2.png";
-import IvantageLogo from "../../assets/images/ivantage logo 1.svg";
-import doctor from "../../assets/images/image 17 1.png";
+import Woman from "../../assets/images/image17.png";
+import Man from "../../assets/images/image15.svg";
+import IvantageLogo2 from "../../assets/images/ivantagelogo2.png";
+import IvantageLogo from "../../assets/images/ivantagelogo1.svg";
+import doctor from "../../assets/images/image171.png";
 import apple from "../../assets/images/pngwing 1.png";
 import google from "../../assets/images/pngwing 2.png";
 import cardBackground from "../../assets/images/Card-1.png"
